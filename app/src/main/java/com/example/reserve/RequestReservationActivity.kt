3e08a71a15bc007bf4d20aaf66778e509790cac7
jot-jt <91110564+jot-jt@users.noreply.github.com>
2021-11-20 13:12:17 -0500

    Added datePicker

--- app/src/main/java/com/example/reserve/RequestReservationActivity.kt
+++ app/src/main/java/com/example/reserve/RequestReservationActivity.kt
@@ -4,15 +4,20 @@ import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.format.DateUtils
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.ImageButton
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.MaterialDatePicker
 
 class RequestReservationActivity : AppCompatActivity() {
 
     private lateinit var backButton: ImageButton
     private lateinit var reserveButton: Button
     private lateinit var checkBox: CheckBox
+    private lateinit var startDate: Button
+    private lateinit var endDate: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,9 +26,41 @@ class RequestReservationActivity : AppCompatActivity() {
         backButton = findViewById(R.id.backButton)
         reserveButton = findViewById(R.id.reserveButton)
         checkBox = findViewById(R.id.checkBox)
+        startDate = findViewById(R.id.startDate)
+        endDate = findViewById(R.id.endDate)
 
         reserveButton.isEnabled = false
         reserveButton.setBackgroundColor(Color.GRAY)
+
+        val fragmentManager = supportFragmentManager
+
+        val today = MaterialDatePicker.todayInUtcMilliseconds()
+
+        val calendarConstraints = CalendarConstraints.Builder()
+            .setStart(today)
+            .setEnd(today + DateUtils.YEAR_IN_MILLIS)
+            // .setValidator() // TODO: Add date validator
+
+        val datePicker = MaterialDatePicker.Builder.datePicker()
+            .setTitleText("Select date")
+            .setSelection(today)
+            .setCalendarConstraints(calendarConstraints.build())
+            .build()
+
+
+
+        datePicker.addOnPositiveButtonClickListener {
+            startDate.text = datePicker.headerText
+            endDate.text = datePicker.headerText
+        }
+
+        startDate.setOnClickListener {
+            datePicker.show(fragmentManager, "DATE_PICKER")
+        }
+
+        endDate.setOnClickListener {
+            datePicker.show(fragmentManager, "DATE_PICKER")
+        }
 
         backButton.setOnClickListener {
             val intent = Intent(this, ExpandedCardActivity::class.java).apply {
