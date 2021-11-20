a6fdb6feafabd594fcf57675da0503f27bf995c3
Demetris <dfk45@cornell.edu>
2021-11-19 21:26:28 -0500

    added tab layout

--- app/src/main/java/com/example/reserve/MainActivity.kt
+++ app/src/main/java/com/example/reserve/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.reserve
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+private const val NUM_PAGES = 3
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewPager: ViewPager2
+    private lateinit var tabLayout: TabLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewPager = findViewById(R.id.viewPager)
+        viewPager.adapter = ViewPagerAdapter(this);
+
+        tabLayout = findViewById(R.id.tabLayout)
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            when (position) {
+                0 -> tab.setIcon(R.drawable.search)
+                1 -> tab.setIcon(R.drawable.calendar)
+                2 -> tab.setIcon(R.drawable.profile)
+            }
+        }.attach()
+    }
+
+    private inner class ViewPagerAdapter(activity: MainActivity) : FragmentStateAdapter(activity) {
+        override fun getItemCount(): Int {
+            return NUM_PAGES
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            return HomeFragment.newInstance()
+        }
+
     }
 }
