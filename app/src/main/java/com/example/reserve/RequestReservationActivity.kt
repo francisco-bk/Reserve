a110192ae6fdad76af2d98525bc514a6b2a666b3
jot-jt <91110564+jot-jt@users.noreply.github.com>
2021-11-20 00:11:38 -0500

    add FAB to expanded card

---
+++ app/src/main/java/com/example/reserve/RequestReservationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.reserve
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RequestReservationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_request_reservation)
+    }
+}
