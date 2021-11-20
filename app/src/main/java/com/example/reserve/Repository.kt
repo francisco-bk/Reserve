9ae52c0e85d73a640ecc66b719d52e150b2c6b42
jot-jt <91110564+jot-jt@users.noreply.github.com>
2021-11-20 18:04:49 -0500

    Changed repository

--- app/src/main/java/com/example/reserve/Repository.kt
+++ app/src/main/java/com/example/reserve/Repository.kt
@@ -3,7 +3,7 @@ package com.example.reserve
 class Repository private constructor() {
     companion object {
         val roomList = arrayListOf<Room>()
-        val favorites = arrayListOf<Boolean>()
+        val favorites = arrayListOf<Room>()
         val reservedTimes = arrayListOf<String>()
     }
 }
