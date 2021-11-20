693d3c94203b2d22226afd45b373ff1548d5c2d7
jot-jt <91110564+jot-jt@users.noreply.github.com>
2021-11-19 22:41:54 -0500

    Add expanded card activity

---
+++ app/src/main/java/com/example/reserve/ExpandedCardActivity.kt
@@ -0,0 +1,11 @@
+package com.example.reserve
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExpandedCardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_expanded_card)
+    }
+}
